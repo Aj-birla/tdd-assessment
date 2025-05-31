7c1d4a69f5ead7177c549c1e296a4e900a1a0f20
Ajay Birla <ajaybirla143@gmail.com>
2025-05-31 14:15:09 +0530

    Added spec for  method with one argument

--- spec/lib/utilities/string_calculator_spec.rb
+++ spec/lib/utilities/string_calculator_spec.rb
@@ -1,3 +1,13 @@
 require_relative '../../../lib/utilities/string_calculator'
 RSpec.describe Utilities::StringCalculator do
+  describe '.add' do
+    it 'is defined as a class method' do
+      expect(described_class).to respond_to(:add)
+    end
+
+    it 'expects exactly one argument' do
+      method = described_class.method(:add)
+      expect(method.arity).to eq(1)
+    end
+  end
 end
