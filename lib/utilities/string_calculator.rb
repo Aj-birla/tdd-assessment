785e88b77ca887563b45c3fd8016303e5b2fe038
Ajay Birla <ajaybirla143@gmail.com>
2025-05-31 15:23:37 +0530

    Usecase:4 handled by development

--- lib/utilities/string_calculator.rb
+++ lib/utilities/string_calculator.rb
@@ -2,6 +2,7 @@ module Utilities
   class StringCalculator
     def self.add(numbers_str)
       return 0 if numbers_str.empty?
+      numbers_str.gsub!("\n", ",")
       numbers = numbers_str.split(',').map(&:to_i)
       numbers.sum
     end
