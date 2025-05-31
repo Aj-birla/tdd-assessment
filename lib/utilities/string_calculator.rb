781336ad2a01ae10c4ffefd09cf9f89d33b20786
Ajay Birla <ajaybirla143@gmail.com>
2025-05-31 14:58:31 +0530

    Matched: usecase:3 give support of multiple nums

--- lib/utilities/string_calculator.rb
+++ lib/utilities/string_calculator.rb
@@ -2,7 +2,8 @@ module Utilities
   class StringCalculator
     def self.add(numbers_str)
       return 0 if numbers_str.empty?
-      numbers_str.to_i if numbers_str =~ /^\d+$/
+      numbers = numbers_str.split(',').map(&:to_i)
+      numbers.sum
     end
   end
 end
