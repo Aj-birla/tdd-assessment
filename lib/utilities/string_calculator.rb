fbbbc9229a3bef4b36ca17c7a567b74b42a6bf1b
Ajay Birla <ajaybirla143@gmail.com>
2025-05-31 16:16:06 +0530

    code optimization

--- lib/utilities/string_calculator.rb
+++ lib/utilities/string_calculator.rb
@@ -1,22 +1,35 @@
 module Utilities
   class StringCalculator
-    def self.add(numbers_str)
-      return 0 if numbers_str.empty?
+    class << self
+      def add(numbers_str)
+        return 0 if numbers_str.empty?
 
-      delimiter = ","
-      if numbers_str.start_with?("//")
-        delimiter_section, numbers_str = numbers_str.split("\n", 2)
-        delimiter = delimiter_section[2]
+        numbers = process(numbers_str)
+
+        numbers.sum
       end
 
-      numbers_str.gsub!("\n", delimiter)
-      numbers = numbers_str.split(delimiter).map(&:to_i)
-      negatives = numbers.select { |n| n < 0 }
-      if negatives.any?
-        raise "negative numbers not allowed #{negatives.join(',')}"
+      private
+      def process(numbers_str)
+        delimiter = ","
+        if numbers_str.start_with?("//")
+          delimiter_section, numbers_str = numbers_str.split("\n", 2)
+          delimiter = delimiter_section[2]
+        end
+
+        numbers_str.gsub!("\n", delimiter)
+        numbers = numbers_str.split(delimiter).map(&:to_i)
+
+        validate_negative_values(numbers)
+        numbers
       end
 
-      numbers.sum
+      def validate_negative_values(numbers)
+        negatives = numbers.select { |n| n < 0 }
+        if negatives.any?
+          raise "negative numbers not allowed #{negatives.join(',')}"
+        end
+      end
     end
   end
 end
